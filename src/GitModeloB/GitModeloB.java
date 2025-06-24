871976f5f62872729b1d37e964b96478e4cc3948
mxxrioo <mario.mingorance.martinez@gmail.com>
2025-06-24 22:46:34 +0200

    Primer commit R1

--- src/GitModeloB/GitModeloB.java
+++ src/GitModeloB/GitModeloB.java
@@ -5,6 +5,7 @@ public class GitModeloB {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo.");
 		System.out.println("Hola a tod@s");
+		System.out.println("Primer cambio r1");
 
 	}
 
